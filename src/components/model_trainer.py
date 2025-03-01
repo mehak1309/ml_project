9b80484f9b48ba208780061db2be064d8df180b7
Mehak Singal <mehaksingal13@gmail.com>
2025-03-01 18:09:47 +0530

    add hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -39,16 +39,44 @@ class ModelTrainer:
 
             models = {
                 "Random Forest": RandomForestRegressor(),
-                "Decision Treee": DecisionTreeRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                "K-Neighbors Classifier": KNeighborsRegressor(),
                 "XGBRegressor": XGBRegressor(),
-                "CatBoosting Classifier": CatBoostRegressor(verbose=False),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(), 
             }
 
-            model_report: dict=evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,  models=models)
+            params = {
+                "Decision Tree": {
+                    "criterion": ["squared_error", "friedman_mse", "absolute_error", "poisson"]
+                },
+                "Random Forest": {
+                    "n_estimators": [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    "learning_rate": [.1, .01, .05, .001],
+                    "subsample": [0.6,0.7,0.75,0.8,0.85,0.9],
+                    "n_estimators": [8,16,32,64,128,256]
+                },
+                "Linear Regression": {},
+                "XGBRegressor": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
+
+            model_report: dict=evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,  models=models, params=params)
 
             best_model_score = max(sorted(model_report.values()))
             best_model_name = list(model_report.keys())[
@@ -59,6 +87,8 @@ class ModelTrainer:
 
             if best_model_score<0.6:
                 raise CustomException("No best model found")
+            
+            print(best_model_name)
             
             logging.info(f"Best model found on both training and testing dataset")
 
